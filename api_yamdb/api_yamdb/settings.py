27c7b5d6f430409bbd96c1bf67e19a5be401b84f
Sofya Serpinskaya <sofyaserpinskaya@gmail.com>
2022-04-16 11:53:23 +0300

    hw39

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # os.getenv('SECRET_KEY', default='secretkey')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
